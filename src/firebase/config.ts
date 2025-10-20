9d67811a2ed8ddb5a907c0999e293ab4b478d6ef
SHINU RATHOD <shinukrathod0143@gmail.com>
2025-10-20 05:11:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5281008203-fcbd7",
+  "appId": "1:631166947523:web:c73ed630e9c9cc3e681276",
+  "apiKey": "AIzaSyDYfKA6hjXOjh1eUDxGOIu02FmcQHBnGjM",
+  "authDomain": "studio-5281008203-fcbd7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "631166947523"
+};
